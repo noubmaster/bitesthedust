651a069929c78054f0926e7c1f715ce9ab1cba09
NoubMaster <noubmaster@gmail.com>
2017-10-01 20:16:45 -0300

    consegui pegar o id do div de outras paginas agr tem que conseguir mudar o style e tem que fazer o onsucces nao dar refresh infinito na pagina/ fazendo busca de musicas

--- src/java/controle/BuscaControle.java
+++ src/java/controle/BuscaControle.java
@@ -5,10 +5,34 @@
  */
 package controle;
 
+import dao.BuscaDAO;
+import java.sql.SQLException;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import modelo.Busca;
+
 /**
  *
  * @author Dendi
  */
+@ManagedBean
+@SessionScoped
 public class BuscaControle {
+
+    private List<Busca> data = null;
+    private List<Busca> result = null;
+    private String display;
+
+    @PostConstruct
+    public void init() {
+        try {
+            data = BuscaDAO.getListaMusicas();
+            result = BuscaDAO.getListaMusicas();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
